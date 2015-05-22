268c2540040fb32231bc1147beacc8a2cd56e88d
Jeff Vance <jvance@redhat.com>
2015-05-22 01:04:16 -0700

    script to create subset category files

---
+++ json/create_categories.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Convert the "convertcvs.json" file to separate files for each db category.
+#
+# constants
+CONV_JSON_FILE='convertcsv.json'
+
+# 1st create a file containing all of the categories
+grep type\": $CONV_JSON_FILE | \
+	uniq | \
+	cut -d: -f2 | \
+	sed -e 's/\r//g' -e 's/"//g' >/tmp/uniq_categories
+
+# define an array containing these categories
+readarray categories </tmp/uniq_categories
+
+# remove existing category subset files
+for c in "${categories[@]}"; do
+   f="subset_${c// /_}"
+   rm -f $f
+done
+
+# write each category to a separate file
+for c in "${categories[@]}"; do
+   f="subset_${c// /_}"
+   grep -h -B5 -A1 "$c" $CONV_JSON_FILE >>$f
+done
